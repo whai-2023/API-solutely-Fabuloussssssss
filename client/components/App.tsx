e2a7397ae704e4da50de605c4560bbe08dbe57b0
martin corpus <nasuntao@yahoo.com>
2023-06-08 16:24:33 +1200

    API-solutely fabulous html

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,33 +2,31 @@ import { useState, useEffect } from 'react'
 import { getGreeting } from '../apiClient'
 
 const App = () => {
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-  const [isError, setIsError] = useState(false)
-
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-        setIsError(false)
-      })
-      .catch((err) => {
-        console.log(err)
-        setIsError(true)
-      })
-  }, [count])
+  
 
   return (
     <>
-      {count}
-      <h1>{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
+      <h1 id="header">StarWars</h1>
+    
+
+        <h3 id="characterName">Character Name</h3>
+
+   
+            <div className="characterLogoContainer">
+                <div className="characterLogo">Box 1</div>
+                <div className="characterLogo">Box 2</div>
+                <div className="characterLogo">Box 3</div>
+                <div className="characterLogo">Box 4</div>
+          </div>
+          <div className="arrowKeys">
+              <div className="leftArrow">
+                <i className="fa-regular fa-circle-left fa-shake"></i>
+              </div>
+              <div className="rightArrow">
+              <i className="fa-regular fa-circle-right fa-shake"></i>
+              </div>
+          </div>
+         
     </>
   )
 }
