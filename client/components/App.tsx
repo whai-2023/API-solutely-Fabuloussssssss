fd730c249a8df5aabee20a4dee4b1a671c0f5b26
martin corpus <nasuntao@yahoo.com>
2023-06-08 18:38:43 +1200

    API-solutely fabulous - side scroll

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,74 +1,85 @@
 import { useState, useEffect } from 'react'
-import { getGreeting } from '../apiClient'
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0)
 
-  const characterLogos = document.querySelectorAll('.characterLogo')
-  let currentIndex = 0
+  useEffect(() => {
+    const characterLogos = document.querySelectorAll('.characterLogo')
 
-  function showCurrentLogo() {
-    const logosArray = Array.from(characterLogos) as HTMLElement[]
-    logosArray.forEach((logo: HTMLElement, index: number) => {
-      logo.style.display = index === currentIndex ? 'block' : 'none'
-    })
-  }
-  
-  
-  function showNextLogo() {
-    if (currentIndex < characterLogos.length - 1) {
-      currentIndex++
-    } else {
-      currentIndex = 0
+    const showCurrentLogo = () => {
+      const logosArray = Array.from(characterLogos) as HTMLElement[]
+      logosArray.forEach((logo: HTMLElement, index: number) => {
+        logo.style.display = index === currentIndex ? 'block' : 'none'
+      })
     }
-    showCurrentLogo()
-  }
-  
-  function showPreviousLogo() {
-    if (currentIndex > 0) {
-      currentIndex--
-    } else {
-      currentIndex = characterLogos.length - 1
+
+    const showNextLogo = () => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex < characterLogos.length - 1 ? prevIndex + 1 : 0
+      )
     }
+
+    const showPreviousLogo = () => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : characterLogos.length - 1
+      )
+    }
+
+    const leftArrow = document.querySelector('.leftArrow')
+    const rightArrow = document.querySelector('.rightArrow')
+
+    if (leftArrow) {
+      leftArrow.addEventListener('click', showPreviousLogo)
+    }
+
+    if (rightArrow) {
+      rightArrow.addEventListener('click', showNextLogo)
+    }
+
     showCurrentLogo()
-  }
-  
-  const leftArrow = document.querySelector('.leftArrow')
-  const rightArrow = document.querySelector('.rightArrow')
-  
-  if (leftArrow) {
-    leftArrow.addEventListener('click', showPreviousLogo)
-  }
-  
-  if (rightArrow) {
-    rightArrow.addEventListener('click', showNextLogo)
-  }
-  
-  
-  showCurrentLogo()
+
+    return () => {
+      if (leftArrow) {
+        leftArrow.removeEventListener('click', showPreviousLogo)
+      }
+
+      if (rightArrow) {
+        rightArrow.removeEventListener('click', showNextLogo)
+      }
+    }
+  }, [currentIndex])
 
   return (
     <>
       <h1 id="header">StarWars</h1>
-    
 
-        <h3 id="characterName">Character Name</h3>
+      <h3 id="characterName">Character Name</h3>
 
-   
-            <div className="characterLogoContainer">
-                <div className="characterLogo Active" id="box1">Box 1</div>
-                <div className="characterLogo" id="box2">Box 2</div>
-                <div className="characterLogo" id="box3">Box 3</div>
-                <div className="characterLogo" id="box4">Box 4</div>
+      <div className="characterLogoContainer">
+        <div className="arrowKeys">
+          <div className="leftArrow">
+            <i className="fa-regular fa-circle-left fa-shake"></i>
           </div>
-          <div className="arrowKeys">
-              <div className="leftArrow">
-                <i className="fa-regular fa-circle-left fa-shake"></i>
-              </div>
-              <div className="rightArrow">
-              <i className="fa-regular fa-circle-right fa-shake"></i>
-              </div>
+
+          <div className="characterLogo Active" id="box1">
+            Box 1
           </div>
-         
+          <div className="characterLogo" id="box2">
+            Box 2
+          </div>
+          <div className="characterLogo" id="box3">
+            Box 3
+          </div>
+          <div className="characterLogo" id="box4">
+            Box 4
+          </div>
+
+          <div className="rightArrow">
+            <i className="fa-regular fa-circle-right fa-shake"></i>
+          </div>
+        </div>
+      </div>
+      
     </>
   )
 }
